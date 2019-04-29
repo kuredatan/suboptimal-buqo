cac7ec1718c5a450b95149cef2940cdaadfcf750
root <root@localhost.localdomain>
2019-04-29 15:24:43 +0000

    fix dill

--- tests.py
+++ tests.py
@@ -14,7 +14,8 @@ def robustness(x):
 		res = main(x, sigma=sigma, true_sigma=true_sigma, print_stuff=False)
 		rhos.append(res[0])
 		vs.append(res[1])
-		pickle.dump({'nombre': vs, 'rhos': rhos, 'sigma_values': sigma_values, 'current_sigma': sigma}, "intermediary_robustness.pi", "wb")
+		with open("intermediary_robustness.pi", "wb") as f:
+			pickle.dump({'nombre': vs, 'rhos': rhos, 'sigma_values': sigma_values, 'current_sigma': sigma}, f)
 	plt.plot([sigma-true_sigma for sigma in sigma_values], rhos, col=["g+" if (v==1) else "r+" for v in vs], title="Evolution de rho_alpha\nen fonction de sigma-true_sigma\n(true_sigma=" + str(true_sigma)+")")
 	plt.savefig("robustness.jpg", bbox_inches="tight")
 	plt.show()
@@ -27,10 +28,12 @@ def calibration(x):
 		print(">> Iteration #" + str(i+1))
 		res = main(x, sigma=sigma, print_stuff=False)
 		vs.append(res[1])
-		pickle.dump({'nombre': vs, 'niter': i}, "intermediary_calibration.pi", "wb")
+		with open("intermediary_calibration.pi", "wb") as f:
+			pickle.dump({'nombre': vs, 'niter': i}, f)
 	print("Nombre empirique :", round(100*sum(vs)/niter,1))
 	print("Valeur théorique :", round(100*alpha, 1))
-	pickle.dump({'empirique': 100*sum(vs)/niter, 'theorique': 100*alpha, 'niter': niter}, "calibration.pi", "wb")
+	with open("calibration.pi", "wb") as f:
+		pickle.dump({'empirique': 100*sum(vs)/niter, 'theorique': 100*alpha, 'niter': niter}, f)
 
 if __name__ == '__main__':
 	di = {'robustness': robustness, 'calibration': calibration}
